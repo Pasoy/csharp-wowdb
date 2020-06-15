1ef3a21f61aefaedfc0894e0dcbd86d8085bccd8
Pasoy <pascal34287@gmail.com>
2020-06-15 12:33:43 +0200

    enable migrations, character model

---
+++ WoWDB_Web/WoWDB_Web/Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WoWDB_Web.Models
+{
+    // POCO
+    public class Character
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Class { get; set; }
+        public String Race { get; set; }
+    }
+}
