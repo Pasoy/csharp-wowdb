23d0d5144960679536de481d469c1d15f10d8962
ADRL <42775975+ADRLxd@users.noreply.github.com>
2020-06-18 19:14:57 +0200

    Models Achievements Creatures NPC Spells

---
+++ WoWDB_Web/WoWDB_Web/Models/Achievements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WoWDB_Web.Models
+{
+    public class Achievements
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Requirements { get; set; }
+        public String Rewards { get; set; }
+        public int AchievementPoints { get; set; }
+    }
+}
---
+++ WoWDB_Web/WoWDB_Web/Models/NPC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WoWDB_Web.Models
+{
+    public class NPC
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Class { get; set; }
+        public String Race { get; set; }
+        public int Faction { get; set; }
+        public int modelid { get; set; }
+        public int health { get; set; }
+        public int mana { get; set; }
+    }
+}
