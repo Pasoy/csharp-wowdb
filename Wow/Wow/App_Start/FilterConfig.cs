7e18e58c17f89b54f50a105c291b99873e9a4003
Pasoy <pascal34287@gmail.com>
2020-06-24 22:56:03 +0200

    https n stuff

--- Wow/Wow/App_Start/FilterConfig.cs
+++ Wow/Wow/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Wow
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
